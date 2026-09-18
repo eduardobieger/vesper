5fc314b54282431acc6ea6ea3f4d40a6f24828a6
Eduardo Bieger <eduardo42bieger@gmail.com>
2026-09-17 22:09:52 -0300

    feat: configure fastify and start server

---
+++ backend/src/server.ts
@@ -0,0 +1,28 @@
+import Fastify from "fastify";
+import { UserRepository } from "./modules/user/user.repository.js";
+import { db } from "./config/db.js";
+import { UserService } from "./modules/user/user.service.js";
+import { UserController } from "./modules/user/user.controller.js";
+import { userRoutes } from "./modules/user/user.routes.js";
+
+const PORT = 3000;
+
+const fastify = Fastify({
+  logger: true,
+});
+
+const userRepository = new UserRepository(db);
+const userService = new UserService(userRepository);
+const userController = new UserController(userService);
+
+await fastify.register(userRoutes, {
+  controller: userController,
+});
+
+fastify.listen({ port: PORT, host: "0.0.0.0" }, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`Server listening on ${{ address }}`);
+});
