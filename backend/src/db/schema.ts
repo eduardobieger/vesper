82cf1bd02034311b9f3b3e61ddff92e26ee48dbd
Eduardo Bieger <eduardo42bieger@gmail.com>
2026-09-17 22:08:51 -0300

    feat: create schema for users table

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey(),
+  firstName: text("first_name").notNull(),
+  lastName: text("last_name").notNull(),
+  email: text("email").notNull(),
+  passwordHash: text("password_hash").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
