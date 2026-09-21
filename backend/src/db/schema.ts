4e40aa27fe00df71b2c92701ba38bc05d786840e
Eduardo Bieger <eduardo42bieger@gmail.com>
2026-09-21 17:51:52 -0300

    feat(schema): create projects table

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,3 +1,4 @@
+import { defineRelations } from "drizzle-orm";
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
@@ -9,3 +10,25 @@ export const users = pgTable("users", {
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
+
+export const projects = pgTable("projects", {
+  id: uuid("id").primaryKey(),
+  name: text("name").notNull(),
+  ownerId: uuid("owner_id")
+    .notNull()
+    .references(() => users.id),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const relations = defineRelations({ users, projects }, (r) => ({
+  projects: {
+    owner: r.one.users({
+      from: r.projects.ownerId,
+      to: r.users.id,
+    }),
+  },
+  users: {
+    projects: r.many.projects(),
+  },
+}));
